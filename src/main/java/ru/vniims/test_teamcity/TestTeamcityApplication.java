71cfaa773cecc0596514735926adb8d168b49abb
Alex <telesyn73@mail.ru>
2020-12-20 10:27:31 +0300

    commit 0

---
+++ src/main/java/ru/vniims/test_teamcity/TestTeamcityApplication.java
@@ -0,0 +1,14 @@
+package ru.vniims.test_teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTeamcityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTeamcityApplication.class, args);
+        System.out.println("Hello world!!!");
+    }
+
+}
---
+++ src/test/java/ru/vniims/test_teamcity/TestTeamcityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vniims.test_teamcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTeamcityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
